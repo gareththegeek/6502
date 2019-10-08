a971b4ee1ff41c1fc9e1ac940b7958f8d69404d4
gareththegeek <gj_williams2000@hotmail.co.uk>
2019-10-08 21:05:35 +0100

    Implemented rti instruction

--- src/6502/pure/operations/rti.ts
+++ src/6502/pure/operations/rti.ts
@@ -1,4 +1,19 @@
 import IState from '../../state/istate'
 import IBus from '../../../bus/ibus'
+import getStackAddress from '../../../bitwise/getStackAddress'
+import getByteStatus from '../getByteStatus'
+import littleEndian from '../../../bitwise/littleEndian'
 
-export default () => (state: IState, bus: IBus, parameter: number): IState => state
+export default () => (state: IState, bus: IBus, _: number): IState => {
+    const byte = bus.read({ address: getStackAddress(state.sp + 1) })
+    const status = getByteStatus(byte)
+    const hi = bus.read({ address: getStackAddress(state.sp + 2) })
+    const lo = bus.read({ address: getStackAddress(state.sp + 3) })
+    const pc = littleEndian([lo, hi])
+    return {
+        ...state,
+        pc,
+        sp: state.sp + 3,
+        status
+    }
+}
---
+++ test/unit/6502/operations/nop.ts
@@ -0,0 +1,20 @@
+import nop from "../../../../src/6502/pure/operations/nop"
+import { build6502State } from "../../../helpers/factories"
+import IBus from "../../../../src/bus/ibus"
+import { expect } from "chai"
+
+describe('Unit', () => {
+    describe('6502', () => {
+        describe('nop', () => {
+            it('should not modify state', () => {
+                const previous = build6502State()
+                
+                const uut = nop()
+                const actual = uut(previous, {} as IBus, 0x00)
+
+                expect(actual).to.be.deep.equal(previous)
+                expect(actual).not.to.be.equal(previous)
+            })
+        })
+    })
+})
