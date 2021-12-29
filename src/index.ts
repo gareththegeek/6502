edf6118b2661adc51945b29c6255fd5fa345a0f4
gareththegeek <gj_williams2000@yahoo.co.uk>
2021-12-29 08:36:57 +0000

    Package project up

--- src/index.ts
+++ src/index.ts
@@ -4,30 +4,25 @@ import cpuFactory from './6502/factory'
 import memoryFactory from './memory/factory'
 import romFactory from './rom/factory'
 import rangeFactory from './rangedcomponent/factory'
+import I6502 from './6502/I6502'
+import IRangedComponent from './rangedcomponent/irangedcomponent'
 
-function main(): void {
+export interface I6502System {
+    cpu: I6502
+    memory: IRangedComponent
+    rom: IRangedComponent
+}
+
+export default (): I6502System => {
     const pubsub = pubSubFactory()
     const bus = busFactory(pubsub)
     const cpu = cpuFactory(bus)
-    const memory = rangeFactory(pubsub, memoryFactory({ from: 0x2000, to: 0x4000 }))
-    const rom = rangeFactory(pubsub, romFactory({ from: 0xfffc, to: 0xfffd }))
+    const memory = rangeFactory(pubsub, memoryFactory({ from: 0x0000, to: 0x4000 }))
+    const rom = rangeFactory(pubsub, romFactory({ from: 0x8000, to: 0xffff }))
 
-    memory.initialise(0x20)
-    rom.initialise([0x00, 0x20])
-
-    cpu.reset()
-    cpu.clock()
-    cpu.clock()
-    cpu.clock()
-    cpu.clock()
-    cpu.clock()
-    cpu.clock()
-    cpu.clock()
-    cpu.clock()
-
-    memory.write({ address: 0x2002, value: 0xbe })
-    const actual = memory.read({ address: 0x2002 })
-    console.log(actual)
+    return {
+        cpu,
+        memory,
+        rom
+    }
 }
-main()
-//document.body.appendChild(component())
