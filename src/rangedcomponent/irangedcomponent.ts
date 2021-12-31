d50e34b4f24aa3c23f2913a5114a2cc1a164efda
gareththegeek <gj_williams2000@yahoo.co.uk>
2021-12-31 09:31:52 +0000

    Return new state from ranged component initialise function

--- src/rangedcomponent/irangedcomponent.ts
+++ src/rangedcomponent/irangedcomponent.ts
@@ -3,11 +3,12 @@ import { TBusRead } from '../bus/state/tbusread'
 import { TBusWrite } from '../bus/state/tbuswrite'
 import { TRangeRead } from '../bus/state/trangeread'
 import IRange from './state/irange'
+import IState from './state/istate'
 
 export default interface IRangedComponent {
     range: IRange
     component: IRangedComponent
-    initialise: (...args: any) => void
+    initialise: (...args: any) => IState
     read: TBusRead
     write: TBusWrite
     readRange: TRangeRead
--- src/rangedcomponent/pure/initialise.ts
+++ src/rangedcomponent/pure/initialise.ts
@@ -2,6 +2,6 @@
 import IState from '../state/istate'
 import IRangedComponent from '../irangedcomponent'
 
-export default (component: IRangedComponent) => (_: IState, ...args: any): any => {
+export default (component: IRangedComponent) => (_: IState, ...args: any): IState => 
     component.initialise(...args)
-}
+
